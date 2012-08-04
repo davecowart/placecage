de573ac68d1fd94c2ad264a67f37d528cdc7129f
Dave Cowart <davecowart@gmail.com>
2012-08-03 22:24:24 -0500

    save resized file, pull random source file

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,10 +1,20 @@
 class ImageController < ApplicationController
 	def show
-		width = params[:width]
-		height = params[:height]
-		image_original = Magick::Image.read(Rails.root.join('images','123.jpeg')).first
-		image = image_original.resize_to_fill(width.to_i,height.to_i)
+		filename = get_image_filename(params[:width], params[:height])
+		image = Magick::Image.read(filename).first
 		response.headers["Content-Type"] = image.mime_type
 		render :text => image.to_blob
+	end
+
+private
+	def get_image_filename(width, height)
+		filename = Rails.root.join('images','generated',"#{width}x#{height}.jpg")
+		return filename if FileTest.exists?(filename)
+
+		original_filename = Dir.glob(Rails.root.join('images','source','*.*')).sample
+		image_original = Magick::Image.read(original_filename).first
+		image = image_original.resize_to_fill(width.to_i,height.to_i)
+		image.write(filename)
+		filename
 	end
 end
