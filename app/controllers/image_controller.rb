9be13aedc36962d9c6b8acfc5772718592f6ec39
Dave Cowart <davecowart@gmail.com>
2012-08-03 22:59:30 -0500

    limit max size to 2000px

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,15 +1,24 @@
 class ImageController < ApplicationController
+	before_filter :check_sizes, :only => [:show, :show_gray]
+
 	def show
-		return_image(params,false)
+		return_image(@width,@height)
 	end
 
 	def show_gray
-		return_image(params,true)
+		return_image(@width,@height,true)
 	end
 
 private
-	def return_image(params, grayscale=false)
-		filename = get_image_filename(params[:width], params[:height], grayscale)
+	def check_sizes
+		@width = params[:width].to_i
+		@height = params[:height].to_i
+		return render :nothing => true, :status => 400 if @height == nil || @width == nil || @height == 0 || @width == 0
+		return render :nothing => true, :status => 403 if @height > 2000 || @width > 2000
+	end
+
+	def return_image(width, height, grayscale=false)
+		filename = get_image_filename(width, height, grayscale)
 		image = Magick::Image.read(filename).first
 		response.headers["Content-Type"] = image.mime_type
 		render :text => image.to_blob
@@ -21,7 +30,7 @@ private
 
 		original_filename = Dir.glob(Rails.root.join('images','source','*.*')).sample
 		image_original = Magick::Image.read(original_filename).first
-		image = image_original.resize_to_fill(width.to_i,height.to_i)
+		image = image_original.resize_to_fill(width,height)
 		image = image.quantize(256,Magick::GRAYColorspace) if grayscale
 		image.write(filename)
 		filename
