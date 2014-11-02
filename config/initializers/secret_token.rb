4239bf0f7001aab6a4692ebd8f854be647a0755d
Dave Cowart <davecowart@gmail.com>
2014-11-02 10:03:11 -0600

    clean house for sinatra migration

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-PlaceCage::Application.config.secret_token = 'c29ec5b0358dd6bbdee8193f25f69a5295020ac582640cad6f2b9bf46b91a52410fa9716ba57cd67b772f7831e2c368241aa45c9daa36d10c4fa8945cde469a3'
