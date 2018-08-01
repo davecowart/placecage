834629610092d585f47e6683efb8d704ead5034d
Dave Cowart <davecowart@gmail.com>
2018-08-01 12:06:24 -0500

    finalize multi-site support

--- myapp.rb
+++ myapp.rb
@@ -2,44 +2,59 @@ set :public_folder, File.dirname(__FILE__) + '/public'
 set :generated_images_folder, File.dirname(__FILE__) + '/images/generated'
 set :images_folder, File.dirname(__FILE__) + '/images/source'
 set :static_cache_control, [:public, :max_age => 300]
-gabba = Gabba::Gabba.new("UA-37788508-2", "auto")
+gabba = nil
+domain = nil
 
 before do
+  domain = request.host.split('.').reverse[1]
+
+  #set gabba
+  ga_index = 1
+  case domain
+  when 'fillmurray'
+    ga_index = 1
+  when 'placecage'
+    ga_index = 2
+  when 'stevensegallery'
+    ga_index = 3
+  end
+  gabba = Gabba::Gabba.new("UA-37788508-#{ga_index}", "auto")
+
   pass if request.path. == '/'
   cache_control :public, :max_age => 31536000
   check_sizes
 end
 
 get '/' do
-  send_file File.join(settings.public_folder, 'index.html')
+  send_file File.join(settings.public_folder, domain, 'index.html')
 end
 
 get '/:width/:height' do
   width = params[:width].to_i
   height = params[:height].to_i
   gabba.page_view("Show", "#{width}/#{height}")
-  return_image(width, height)
+  return_image(domain,width,height)
 end
 
 get '/c/:width/:height' do
   width = params[:width].to_i
   height = params[:height].to_i
   gabba.page_view("ShowCrazy", "c/#{width}/#{height}")
-  return_image(width,height,:crazy)
+  return_image(domain,width,height,:crazy)
 end
 
 get '/g/:width/:height' do
   width = params[:width].to_i
   height = params[:height].to_i
   gabba.page_view("ShowGray", "g/#{width}/#{height}")
-  return_image(width,height,:grayscale)
+  return_image(domain,width,height,:grayscale)
 end
 
 get '/gif/:width/:height' do
   width = params[:width].to_i
   height = params[:height].to_i
   gabba.page_view("ShowGif", "gif/#{width}/#{height}")
-  return_gif(width,height)
+  return_gif(domain,width,height)
 end
 
 private
@@ -51,23 +66,23 @@ private
     raise error 'Too Large' if height > 3500 || width > 3500
 	end
 
-	def return_image(width, height, *args)
+	def return_image(domain, width, height, *args)
 		grayscale = args.include?(:grayscale)
 		crazy = args.include?(:crazy)
-		filename = get_image_filename(width, height, grayscale, crazy)
+		filename = get_image_filename(domain, width, height, grayscale, crazy)
     send_file filename, type: 'image/jpeg', disposition: 'inline'
 	end
 
-  def return_gif(width, height)
-    send_file get_gif_filename(width, height), type: 'image/gif', disposition: 'inline'
+  def return_gif(domain, width, height)
+    send_file get_gif_filename(domain, width, height), type: 'image/gif', disposition: 'inline'
   end
 
-  def get_gif_filename(desired_width, desired_height)
-    original_path = ['gifs', '*.gif']
+  def get_gif_filename(domain, desired_width, desired_height)
+    original_path = [domain, 'gifs', '*.gif']
     filename = Dir.glob(File.join(settings.images_folder, *original_path)).sample
 
     dimensions = "#{desired_width.to_i}x#{desired_height.to_i}"
-    path = ['gifs',"#{dimensions}.gif"]
+    path = [domain, 'gifs',"#{dimensions}.gif"]
     generated_filename = File.join(settings.generated_images_folder, *path)
 		return generated_filename if FileTest.exists?(generated_filename)
 
@@ -97,8 +112,8 @@ private
     generated_filename
   end
 
-	def get_image_filename(width, height, grayscale=false, crazy=false)
-		path = []
+	def get_image_filename(domain, width, height, grayscale=false, crazy=false)
+    path = [domain]
 		path << 'grayscale' if grayscale
 		path << 'crazy' if crazy
 		path << "#{width}x#{height}.jpg"
@@ -107,7 +122,7 @@ private
     filename = File.join(settings.generated_images_folder, *path)
 		return filename if FileTest.exists?(filename)
 
-		original_path = []
+    original_path = [domain]
 		original_path << 'crazy' if crazy
 		original_path << '*.*'
     original_filename = Dir.glob(File.join(settings.images_folder, *original_path)).sample
